52c0760eaa5259d670f629a323ecf0daa3199dc7
Sourit Chakraborty <sourchak@gmail.com>
2019-03-26 17:09:30 +0530

    Only Word2Vec left

--- master.py
+++ master.py
@@ -76,9 +76,9 @@ def cover_preprocessing(option):
                 #for x in list(cover):
                 #    print(x)
                 cover_text, loc_contexts=preprocessing(cover)
-                for x in loc_contexts.keys():
-                    print(str(x)+'->',end='')
-                    print(loc_contexts[x])
+                #for x in loc_contexts.keys():
+                #    print(str(x)+'->',end='')
+                #    print(loc_contexts[x])
                 return cover_text,loc_contexts
             else:
                 print(option.capitalize() +' text file does not exist, or you do not have required premissions on the file.')
@@ -86,17 +86,45 @@ def cover_preprocessing(option):
             print('An error occured while trying to read the cover file.')
 
 def octalify(raw_message):
-    # convert the message to its special octal form 
-    return raw_message
+    # convert the message to its special octal form
+    message=''
+    punc=string.punctuation.translate(None,'~#%<=>^')+' '
+    for x in raw_message:
+        if x.isdigit():
+            message=message+str(oct(ord(x)-ord('0')))[1:].rjust(2,'0') # 0,1,2,3,4,5,6,7,8,9 for digits
+        elif x.isalpha():
+            message=message+str(oct(ord(x.upper())-ord('A')+10))[1:] # 10,11,12,... for alphabets
+        elif x in punc:
+            #print('Inside octalify: '+str(oct(punc.find(x)+36))[1:])
+            message=message+str(oct(punc.find(x)+36))[1:]
+        else:
+            print(x+' is an unsupported character.')
+    message=message+'7777' # boundary condition
+    #print (message)
+    return  message
 
 def inv_octalify(octal_message):
-    return octal_message
+    message=''
+    x=0
+    punc=string.punctuation.translate(None,'~#%<=>^')+' '
+    #print(len(octal_message))
+    while x<len(octal_message):
+        #print(octal_message[x:x+2])
+        m=int(octal_message[x:x+2],8)
+        if m<10:
+            message=message+chr(m+ord('0'))
+        elif m<36:
+            message=message+chr(m+ord('A')-10)
+        elif m<62:
+            message=message+punc[m-36]
+        x=x+2
+    return message
 
 def read_message():
     message_file=raw_input('Path to message: ')
     if path.exists(path.abspath(message_file)):
         raw_message=open(path.abspath(message_file),'r')
-        message=octalify(list(raw_message))
+        message=octalify(raw_message.read())
         return message
     else:
         print('Message file does not exist, or you do not have required permissions on the file')
@@ -111,15 +139,14 @@ def embed(cover_text,loc_contexts,message):
     msg_ptr=0
     end=0
     log='' # this will be changed once word2vec is mereged with this.
-    message_vec=list(message[0]) # temporary
     for loc in sorted(loc_contexts):
-        if msg_ptr<len(message_vec):
+        if msg_ptr<len(message):
             words=word2vecEvaluator(log,loc_contexts[loc])
             # print('loc='+str(loc), msg_ptr, message_vec[msg_ptr])
-            loc_words[loc]=words[int(message_vec[msg_ptr])]
+            loc_words[loc]=words[int(message[msg_ptr])]
             msg_ptr=msg_ptr+1
             end=loc
-    #TO DO: Take care of End of Message decided to be 777 in octal
+    #TO DO: Take care of End of Message decided to be 7777 in octal
     for loc in sorted(loc_words):
         # print('loc='+str(loc))
         cover_text[loc]=loc_words[loc]
@@ -133,17 +160,24 @@ def embed(cover_text,loc_contexts,message):
 def extractor(cover_text,loc_contexts):
     flag=True
     log='' # like embed, this too will be changed once word2vec is merged 
-    consec_seven=0 #to keep track of consecutive 7s, 3 7s assumed to signal message end
+    consec_seven=0 #to keep track of consecutive 7s, 4 7s assumed to signal message end
+    octal_message=''
     for loc in sorted(loc_contexts):
         if flag:
             words=word2vecEvaluator(log,loc_contexts[loc])
-            pos=words.find(cover_text[loc])
+            pos=words.index(cover_text[loc])
             octal_message=octal_message+str(pos)
             if pos==7:
-                consec_seven=consec_seven+1
-                if consec_seven==3:
+                if len(octal_message)%2==1 and consec_seven==0:
+                    consec_seven=1
+                elif consec_seven>0:
+                    consec_seven=consec_seven+1
+            elif pos!=7:
+                consec_seven=0
+            if consec_seven==4:
                     flag=False
-            else: cosec_seven=0
+    if not flag:
+        octal_message=octal_message[:-4]
     message=inv_octalify(octal_message)
     with open('Extracted_Message.txt','w') as extrctd_msg:
         extrctd_msg.write(message)
@@ -157,4 +191,4 @@ if choice=='embed':
     embed(cover_text,loc_contexts,message)
 elif choice=='extract':
     cover_text,loc_contexts=cover_preprocessing('stego')
-    message=extract(cover_text,loc_contexts)
+    message=extractor(cover_text,loc_contexts)
